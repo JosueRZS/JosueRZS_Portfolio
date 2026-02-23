60a5d049fb2a4410160d3bb4f0e3dbe3c4003ad5
Josue Ruiz <josuerzsdev@gmail.com>
2026-02-23 15:33:41 -0500

    cambio en el estilo del footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,12 +2,12 @@
 
 export default function Footer() {
   return (
-    <footer className="mt-24 pt-8 border-t border-zinc-900 flex flex-col md:flex-row justify-between items-center gap-4 text-sm text-zinc-600">
+    <footer className="mt-24 pt-8 border-t border-zinc-900 flex flex-col md:flex-row justify-between items-center gap-4 text-sm text-zinc-400">
       <p>© {new Date().getFullYear()} Josue RZS</p>
       <div className="flex gap-6">
-        <a href="https://github.com/JosueRZS" target="_blank" className="hover:text-white transition-colors">GitHub</a>
-        <a href="https://www.linkedin.com/in/josue-ruiz-0952001b3/" target="_blank" className="hover:text-white transition-colors">LinkedIn</a>
-        <a href="https://www.instagram.com/josue_rzs/" target="_blank" className="hover:text-white transition-colors">Instagram</a>
+        <a href="https://github.com/JosueRZS" target="_blank" className="hover:text-white hover:border-b hover:border-b-white hover:transition-all hover:duration-200">GitHub</a>
+        <a href="https://www.linkedin.com/in/josue-ruiz-0952001b3/" target="_blank" className="hover:text-white hover:border-b hover:border-b-white hover:transition-all hover:duration-200">LinkedIn</a>
+        <a href="https://www.instagram.com/josue_rzs/" target="_blank" className="hover:text-white hover:border-b hover:border-b-white hover:transition-all hover:duration-200">Instagram</a>
       </div>
     </footer>
   );
